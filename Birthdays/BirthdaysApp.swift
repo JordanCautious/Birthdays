c2a8d27d2571f89635e150900dcba0c37f016d2d
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-15 12:46:05 -0400

    Completd the application

--- Birthdays/BirthdaysApp.swift
+++ Birthdays/BirthdaysApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BirthdaysApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Friend.self)
         }
     }
 }
