d235c050fc69959020f8d7bb8dc3ade372236ed7
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-15 12:13:54 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Jordan Haynes on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthdays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birthdays
+//
+//  Created by Jordan Haynes on 7/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
